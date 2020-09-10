0858b6951ff6a04f804a5fd2dde7af378f9f7dc7
tsuchiyoshi <tsuchiyoshi049@gmail.com>
2020-09-10 13:47:28 +0900

    create bitbank_api.py

--- test_1.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-urls = ["https://public.bitbank.cc/btc_jpy/ticker", \
-        "https://public.bitbank.cc/eth_btc/ticker", \
-        "https://public.bitbank.cc/mona_jpy/ticker"]
-
-print(help(requests))
