c78a83ae7a4079741f1d25070b580a91251950e5
tsuchiyoshi <tsuchiyoshi049@gmail.com>
2020-09-10 11:37:29 +0900

    create develop branch

---
+++ test_1.py
@@ -0,0 +1,7 @@
+import requests
+
+urls = ["https://public.bitbank.cc/btc_jpy/ticker", \
+        "https://public.bitbank.cc/eth_btc/ticker", \
+        "https://public.bitbank.cc/mona_jpy/ticker"]
+
+print('testtesttest')
